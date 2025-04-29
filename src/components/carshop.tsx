9f59995a5fa680958324b88e5131ec0a01721ae2
sdfunes <sdfunes@gmail.com>
2025-04-28 22:01:56 -0300

    feat: ultimas mejoras

--- src/components/carshop.tsx
+++ src/components/carshop.tsx
@@ -1,27 +1,36 @@
-import React from "react";
+import React, { useState } from 'react';
 
 function CarShop() {
-  const cars = [
-    { id: 1, name: "Car A", price: "$20,000", image: "car-a.jpg" },
-    { id: 2, name: "Car B", price: "$25,000", image: "car-b.jpg" },
-    { id: 3, name: "Car C", price: "$30,000", image: "car-c.jpg" },
+  const [cart, setCart] = useState([]);
+  const remes = [
+    { id: 1, name: 'Remera 1', price: '$20,000', image: 'car-a.jpg' },
+    { id: 2, name: 'Remera 2', price: '$25,000', image: 'car-b.jpg' },
+    { id: 3, name: 'Remera 3', price: '$30,000', image: 'car-c.jpg' },
   ];
 
+  const addToCart = (item) => {
+    setCart((prevCart) => [...prevCart, item]);
+    alert(`${item.name} agregado al carrito!`);
+  };
+
   return (
-    <div className="mt-8 p-4 border rounded shadow">
-      <h2 className="text-2xl font-semibold mb-4">Car Shop</h2>
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-        {cars.map((car) => (
-          <div key={car.id} className="border p-4 rounded shadow">
+    <div className='mt-8 p-4 border rounded shadow'>
+      <h2 className='text-2xl font-semibold mb-4'>Tienda</h2>
+      <div className='grid grid-cols-1 md:grid-cols-3 gap-4'>
+        {remes.map((rem) => (
+          <div key={rem.id} className='border p-4 rounded shadow'>
             <img
-              src={car.image}
-              alt={car.name}
-              className="w-full h-40 object-cover mb-2"
+              src={rem.image}
+              alt={rem.name}
+              className='w-full h-40 object-cover mb-2'
             />
-            <h3 className="text-lg font-bold">{car.name}</h3>
-            <p className="text-gray-600">{car.price}</p>
-            <button className="mt-2 px-4 py-2 bg-blue-500 text-white rounded">
-              Add to Cart
+            <h3 className='text-lg font-bold'>{rem.name}</h3>
+            <p className='text-gray-600'>{rem.price}</p>
+            <button
+              className='mt-2 px-4 py-2 bg-blue-500 text-black rounded'
+              onClick={() => addToCart(rem)}
+            >
+              Agregar al carrito
             </button>
           </div>
         ))}
